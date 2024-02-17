2b09f01a67f792866ff13ae3b37e16a4ea68c328
andjeladj0rdjevic <79863542+andjeladj0rdjevic@users.noreply.github.com>
2024-02-17 11:21:58 +0100

    Create Predmeti.py

---
+++ model/Predmeti.py
@@ -0,0 +1,25 @@
+from collections import OrderedDict
+
+class Predmeti:
+    def __init__(self, predmeti):
+        if not predmeti:
+            raise ValueError("Predmeti cannot be null or empty")
+
+        self.predmet_per_name = OrderedDict()  # Preserve insertion order
+        for p in predmeti:
+            self.predmet_per_name[p.naziv.lower()] = p  # Case-insensitive mapping
+
+    def get_nr_of_predmeti(self):
+        return len(self.predmet_per_name)
+
+    def get_by_name(self, name):
+        return self.predmet_per_name.get(name.lower())
+
+    def get_names(self):
+        return list(self.predmet_per_name.keys())
+
+    def get_names_enclosed(self, enclose_with):
+        return [enclose_with + name + enclose_with for name in self.get_names()]
+
+    def get_all(self):
+        return self.predmet_per_name.values()
